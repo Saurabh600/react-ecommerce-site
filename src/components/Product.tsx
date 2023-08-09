266b16702b9e93b5678d470158734d12de789344
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2023-08-09 23:16:23 +0530

    prodcut section improved

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -7,8 +7,8 @@ export default function Product({ data: product }: { data: TProduct }) {
       <div className="product__title">{product.title}</div>
       <div className="product__desc">{product.description}</div>
       <span className="product__price">{product.price}</span>
-      <span className="product__rating">{product.rating.rate}</span>
-      <span className="product__count">{product.rating.count}</span>
+      <div className="product__rating">rating {product.rating.rate}</div>
+      <div className="product__count">reviews {product.rating.count}</div>
     </a>
   )
 }
