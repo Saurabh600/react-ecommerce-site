7cd81eead239e2604c0f44886505ec1365a15436
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2023-08-15 16:11:18 +0530

    hoem page updated

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Products from "../components/Products";
+import { auth } from "../services/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function HomePage() {
+  const [hasLoggedIn, setHasLoggedIn] = useState(false);
+
+  useEffect(() => {
+    setHasLoggedIn(() => (auth.currentUser ? true : false));
+  }, []);
+
+  onAuthStateChanged(auth, () => {
+    setHasLoggedIn(() => (auth.currentUser ? true : false));
+  });
+
   return (
     <>
-      <Navbar />
-      <Products />
+      <Navbar hasLoggedIn={hasLoggedIn} />
+      <main className="main-wrapper">
+        <Products />
+      </main>
+      <Footer />
     </>
   );
 }
