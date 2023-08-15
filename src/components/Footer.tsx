95dff7433d9dc834d9b0cf702ab04e4575a01445
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2023-08-15 16:05:15 +0530

    footer added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-text">
+        Copyright (c) 2023 Saurabh Chaudhary. All Rights Reserved.
+      </p>
+    </footer>
+  );
+}
