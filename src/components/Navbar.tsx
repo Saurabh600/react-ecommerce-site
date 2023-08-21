04d62fe1dcca54530c8a92571b376b94f48e614c
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2023-08-21 18:05:49 +0530

    navbar about link updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/about">
+          <Link className="nav-link" to="/#about">
             About
           </Link>
         </li>
