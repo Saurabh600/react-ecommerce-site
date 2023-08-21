d5822d1c9af2ba1e7fcc01a626343b3b95bfd052
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2023-08-21 18:06:17 +0530

    rating component classname key added

--- src/components/common/Rating.tsx
+++ src/components/common/Rating.tsx
@@ -1,7 +1,12 @@
-export function Rating({ count: rating }: { count: number }) {
+interface Props {
+  count: number;
+  className: string;
+}
+
+export const Rating: React.FC<Props> = ({ count: rating, className }) => {
   const starFull = "⭐";
   const starEmpty = "✰";
   const starArr = new Array(Math.ceil(rating)).fill(starFull);
   if (rating < Math.ceil(rating)) starArr[Math.floor(rating)] = starEmpty;
-  return <div className="product-rating">{starArr.join(" ")}</div>;
+  return <div className={className}>{starArr.join(" ")}</div>;
 }
